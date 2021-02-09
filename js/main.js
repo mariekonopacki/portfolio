b32798641d0b2ac110d364992988c3c64f8bdf18
Marie Konopacki <mariekonopacki@college.harvard.edu>
2021-02-09 11:58:40 -0500

    first push

---
+++ js/main.js
@@ -0,0 +1,200 @@
+// Get viewport dimensions
+var h = $(window).height();
+var w = $(window).width();
+
+gsap.registerPlugin(MotionPathPlugin);
+gsap.registerPlugin(CSSPlugin);
+gsap.registerPlugin(DrawSVGPlugin);
+gsap.registerPlugin(TextPlugin);
+gsap.registerPlugin(ScrollToPlugin);
+gsap.registerPlugin(ScrollTrigger);
+
+loadProjectDisplay();
+
+blobCount = 0;
+animationarray = [];
+
+$(window).on("load", function(){
+
+    loadAnimation();
+    createBlobs();
+
+});
+
+
+function createBlobs() {
+
+    $('.blob').each(function(){
+        blobCount += 1
+        animateBlob("b" + blobCount);
+    });
+
+}
+
+
+function loadAnimation() {
+
+    $('.lines').css('visibility', 'visible')
+    $('.blob-overlay').css('visibility', 'visible')
+
+    gsap.defaultOverwrite = false;
+
+    const loading = gsap.timeline()
+    loading.add('start')
+    loading.fromTo(".background-overlay", {scaleX: 1.1, rotation: 0.01, duration: 0}, {scaleX: 1, rotation: 0.01, duration: 1, ease:"power2.out"}, 'start')
+    loading.from(".blob", {scale: 0, delay: .1, autoAlpha: 0, rotation: 0.01, force3D:true, stagger: .2, duration: 1.5, ease:"power2.out"}, 'start')
+    loading.from('.line', {drawSVG:'0%', delay: .4, duration: .7, stagger: 0.1, ease:"Power2.easeInOut"}, 'start')
+    loading.then(toTwo())
+    loading.then(toThree())
+    loading.then(toFour())
+
+    function toTwo() {
+        const homepage = gsap.timeline({
+            scrollTrigger: {
+                scroller: ".page-scroll",
+                trigger: "#page-1",
+                start: "0%",
+                end: '100%',
+                scrub: true,
+                anticipatePin: 1,
+                toggleActions: "play none none reverse",
+                immediateRender: false
+            }
+        });
+        homepage.add('start')
+        homepage.from('.list-wrapper', {y: '100%', duration: 1, delay: 1.2}, 'start')
+        homepage.from('.project-display', {y: '100%', duration: 2, delay: 1.5}, 'start')
+        homepage.to(".homepage-content", {delay: .5, duration: 1.5, y: -2000, rotation: 0.01, force3D:true}, 'start')
+        homepage.to(".line", {keyframes:[{drawSVG: 0, stroke: 'var(--accent)', duration: .3, force3D:true, stagger: -.1}, {autoAlpha: 0}]}, 'start')
+        homepage.fromTo(".background-overlay", {scaleX: 1, duration: 0, rotation:0.001}, {scaleX: 0, delay: 1, duration: 2, rotation:0.001}, 'start')
+        homepage.to("#overlay-color-rect", { delay: 1, duration: .3}, 'start')
+        homepage.to(".down-arrow", {delay: .2, duration: 1, x: "-100vw"}, 'start')
+        homepage.to(".blob", {keyframes:[{scale: 0.01, stagger: .08, duration: .3}, {autoAlpha: 0}]}, 'start')
+        homepage.to(".blobs", {autoAlpha: 0, duration: 1, delay: 1}, 'start')
+    }
+
+    function toThree() {
+        const illustration = gsap.timeline({
+            scrollTrigger: {
+                scroller: ".page-scroll",
+                trigger: "#page-2",
+                start: "0%",
+                end: "100%",
+                scrub: true,
+                anticipatePin: 1,
+                toggleActions: "play none none reverse"
+            }
+        });
+        illustration.add('start')
+        illustration.from('.illustration', {y: '100%', stagger: 1, duration:2, delay: 2, ease:"power2.out"}, 'start')
+        illustration.fromTo(".background-overlay", {scaleX: 0, duration: 0}, {scaleX: 2, duration: 2}, 'start')
+
+    }
+
+
+    function toFour() {
+        const about = gsap.timeline({
+            scrollTrigger: {
+                scroller: ".page-scroll",
+                trigger: "#page-3",
+                start: "0%",
+                end: "100%",
+                scrub: true,
+                anticipatePin: 1,
+                toggleActions: "play none none reverse",
+                immediateRender: false
+            }
+        });
+        about.add('start')
+        about.fromTo(".background-overlay", {scaleX: 2, duration: 0}, {scaleX: 1, duration: 2, delay: 1}, 'start')
+        about.to("#overlay-color-rect", {fill: "#5d00e0", duration: 1}, 'start')
+        about.to(".blob", {keyframes:[{scale: 1}, {autoAlpha: 1}]}, 'start')
+        about.to(".blobs", {autoAlpha: 1, duration: 1, delay: 1}, 'start')
+    }
+
+
+}
+
+function animateBlob(blob) {
+
+    // Create random points for blob path
+    var newData = d3.range(3).map(function(){
+        return [Math.floor(Math.random() * (w * .4)), Math.floor(Math.random() * h)]
+    });
+
+    // Generate a curved path for smooth blob moving
+    var lineGenerator = d3.line().curve(d3.curveNatural);
+    var pathString = lineGenerator(newData)  + " z";
+
+    var animation = gsap.to("#" + blob, {
+                        motionPath: pathString,
+                        duration: 60,
+                        yoyo: true,
+                        repeat: -1,
+                        ease: "linear",
+                        force3D:true,
+                        rotation: 0.01
+                    });
+
+    animationarray.push(animation);
+}
+
+$('.skill').hover(function() {
+    $(this).attr('style', 'color:var(--homepage); background-position: 0% 50%; background-size: 100% 80%;  transition: color .2s ease-in, background-position .3s, background-size .3s ease-in .1s')
+}, function(){
+    $(this).attr('style', 'background-position: 0% 50%; color: #4f4c4e; background-size: 0% 80%; transition: color .3s, background-size .3s, background-position .3s ease-in;')
+});
+
+function loadProjectDisplay() {
+    $('.project-display').empty();
+
+    let selectedProject = $('.selected').attr('id');
+    switch (selectedProject) {
+        case 'olympics': $('.project-display').append(olympics); break;
+        case 'fashion': loadFashion(); break;
+        case 'jester': $('.project-display').append(jester); break;
+        case 'ai-explorer': $('.project-display').append(aiExplorer); break;
+        case 'novel-art': $('.project-display').append(novelArt); break;
+        case 'womens-issue': $('.project-display').append(womensIssue); break;
+        case 'hueseum': $('.project-display').append(hueseum); break;
+        case 'spirograph': $('.project-display').append(spirograph); break;
+        case 'piano-lamp': $('.project-display').append(pianoLamp); break;
+    }
+
+    gsap.to('.project-display', {scrollTo: 0})
+}
+
+function updateSelected(element) {
+    $('.selected').removeClass('selected')
+    $(element).addClass('selected')
+
+    loadProjectDisplay();
+
+}
+
+
+function scroll1() {
+    gsap.to('.page-scroll', {keyframes: [{scrollSnapType: 'none'}, {scrollTo: {y: "#page-1", duration: 1}, ease: "power2"}, {scrollSnapType: 'y mandatory'}]});
+}
+
+function scroll2() {
+    gsap.to('.page-scroll', {keyframes: [{scrollSnapType: 'none'}, {scrollTo: {y: "#page-2", duration: 1}, ease: "power2"}, {scrollSnapType: 'y mandatory'}]});
+}
+
+function scroll3() {
+    gsap.to('.page-scroll', {keyframes: [{scrollSnapType: 'none'}, {scrollTo: {y: "#page-3", duration: 1}, ease: "power2"}, {scrollSnapType: 'y mandatory'}]});
+}
+
+function scroll4(){
+    gsap.to('.page-scroll', {keyframes: [{scrollSnapType: 'none'}, {scrollTo: {y: "#page-4", duration: 1}, ease: "power2"}, {scrollSnapType: 'y mandatory'}]});
+}
+
+
+
+function loadFashion() {
+    $('.project-display').append(fashion);
+
+    PDFObject.embed("./assets/FSF Case Study _ Marie Konopacki.pdf", "#pdf");
+
+}
+
---
+++ projectDescriptions.js
@@ -0,0 +1,149 @@
+
+var olympics = `
+                <div class="project-image" id="header-image" style="--image-url:url('./assets/olympics/web-banner.png')"></div>
+                <div class="display-section" >
+                    <h2 class="display-date">Fall 2020 -</h2>
+                    <h2 class="section-title">Visualizing Accessibility in the Olympics</h2>
+                </div>
+                <div class="display-section">
+              
+                    <p>For the last two months of CS171: Visualization, I led a team of 3 with the objective of devising a visual, interactive narrative sourced from freely available data.
+                        Finding interest in the Olympic Games, we focused on the structural barriers countries or athletes might face when trying to compete for the gold medal. 
+                        Using data we found, we pinpointed relationships that allowed us to then ideate and unfold complex visualizations to demonstrate informative trends.</p>
+                </div>
+                <div class="display-section">
+                    <h2 class="section-title">Responsibilities</h2>
+                    <p>The three of us each designed and developed several interactive D3 visualizations after writing Javascript programs to access and prepare our data for processing. As the team leader, I also guided our ideas into a cohesive vision in our discussions, structured our timeline and specific goals, and designed the overall appearance of our website while also making sure all of our individual visualizations fit into its theme. </p>
+                </div>
+                <div class="display-section">
+                    <h2 class="section-title">The Data</h2>
+                    <p>We were able to find data of athletes’ physical attributes, hometowns, sport, and medals they won if applicable. We also found data detailing country representation at each game both by athletes sent to compete and medals actually won. To supplement and give context to our data, we additionally located country GDPs over time as well as average income by hometown. We also found the results of a 538 survey that polled US citizens to find the most popular Olympic sport. </p>
+                </div>
+                <div class="display-section">
+                    <h2 class="section-title">Prototyping</h2>
+                    <p>Initially, we identified trends in Tableau, which allowed us to quickly determine the points of our story before we actually dove into programming these visualizations from scratch. Then, using Javascript, we individually created programs accessing data from different sources and processing it into a form D3, a Javascript library for creating visualizations, could interpret. We then created rudimentary graphs that we used to conduct user testing. </p>
+                </div>
+                <div class="display-section">
+                    <h2 class="section-title">User Research</h2>
+                    <p>After building our initial prototype, we conducted interviews to get feedback on our UI and overall flow of the story. We interviewed 3 students, which we found ideal as they were our target audience, and we wanted this information to be, well, accessible! From our testing, we were able to spot and correct interactions and reorder some of the visualizations.</p>
+                </div>
+                <div class="display-section">
+                    <h2 class="section-title">Design</h2>
+                    <p>After iterating through prototypes, we landed on a framework with which we were very happy. From there, I started building cohesion between our visualizations through design. Inspired by the nighttime ceremonies of the Olympics, I went with a darker color palette which also emphasized the data within our graphs. </p>
+                </div>
+                <div class="display-section">
+                    <h2 class="section-title">The Story</h2>
+                    <p>Although not many people can compare themselves to Olympic athletes, we wanted this data to be personal and relatable. We decided to begin our story by asking you to input your own age, gender, height and weight, so you could see what sport you might play if you just so happened to be a world-class athlete. This then segues into a dashboard I created demonstrating the physical attributes of athletes by sport. Following this, the user is then presented with the hometowns of US athletes, which precedes a set of visualizations that analyze the average household income of these hometowns relative to the national median. </p>
+                    <p>Then, taking a step back, we took a look at country data. Inspired by Hans Rosling’s famous 200 Years in 4 minutes presentation, I sought to create my own version by looking at countries’ GDPs compared to the amount of medals they win and animating these trends over time. The findings are striking: in the 1980s, we see non-Western countries become increasingly competitive as their GDPs also rise. In our next visualization, we found that countries that are hosting the games that year have an undeniable advantage.</p>
+                    <p>We then compiled all of our prior findings into a relative scale. Which sports in the Olympics are most accessible? Meaning, which sports have a wider range of athlete physical traits, a more equal male to female ratio, and the range of countries who win medals in that sport. We found that Track and Field won. Taking this new data a step further, I took the survey results of popularity to see if there’s a correlation between a sport’s accessibility and its favorability. In the end, we found that the more accessible sports tend to be the most popular.</p>
+                </div>
+                `
+
+var fashion = `<div class="project-image" id="header-image" style="--image-url:url('./assets/fashion/web-banner.png')"></div>
+                <div class="display-section">
+                    <h2 class="display-date">Fall 2020</h2>
+                    <h2 class="section-title">Fashion Scholarship Fund Case Study</h2>
+                </div>
+            </div>
+            <div class="display-section">
+                <p>Scholarship-winning proposal of sustainable clothing made affordable by bundling foundation pieces</p>
+            </div>
+            <div class="display-section">
+                <h2 class="section-title">Objective</h2>
+                <p>Each year, the Fashion Scholarship Fund competition prompts students to propose a fashion line through a fifteen page slide deck. For 2020, applicants had to develop a line of fashion products that would appeal to Gen Z consumers, based on what the student identified as key purchase drivers.</p>
+            </div>
+            <div class="display-section">
+                <h2 class="section-title">The Problem</h2>
+                <p>Gen Z is a generation uniquely in tune with the social issues of today such as environmental impact and sustainability. But after online research and interviews with 18 of my peers, I found that young people actually consider affordability the most important factor when purchasing clothing. This means questionable fast fashion stores like Forever 21 remain popular for their low prices, even if pricier sustainable clothing is the more desirable option.</p>
+            </div>
+            <div class="display-section">
+                <h2 class="section-title">The Goal</h2>
+                <p>Ideally, the price of sustainable clothing needs to be comparable to fast fashion in order to give Gen Z consumers the financial agency to align their purchases with their philosophies.</p>
+            </div>
+            <div class="display-section">
+                <h2 class="section-title">Proposal</h2>
+                <p>Inspired by bulk retailers such as Costco and Sam’s Club, I developed a ten piece clothing bundle that is designed to be the foundation of anyone’s wardrobe. Since the consumer purchases the pieces together, the price per item would be comparable to fast fashion prices. This is because the company can now operate at wholesale profit margins while also saving on streamlined shipping and handling.</p>
+            </div>
+            <div class="display-section">
+                <h2 class="section-title">Research</h2>
+                <p>In addition to anecdotal conversations with friends, I conducted a Google Form survey in order to capture more precise data. In it, I asked the following questions:</p>
+                <ul>
+                    <li>What stores and websites do you purchase your clothes from the most?</li>
+                    <li>When you’re purchasing clothes, what is most important to you? (Check all that apply)</li>
+                    <li>Between regularly buying clothes that are inexpensive versus more expensive clothing, where do you fall?</li>
+                    <li>How would you describe your fashion sense and how you build your outfits?</li>
+                    <li>How much would you pay for a simple, single-color garment?</li>
+                </ul>
+            </div>
+           
+            <div class="display-section">
+                <h2 class="section-title">Findings</h2>
+                <p>I found that affordability, quality, and uniqueness were the biggest driving factors, while sustainability trailed behind. I found this quite the paradox since various pulse surveys report that 87% of Gen Z is worried about the planet’s health. Eventually, with additional research, I conclude that this disparity does not mean that Gen Z is inauthentic about their beliefs. Rather, it simply points to young people’s growing financial anxiety.</p>
+            </div> 
+            <div class="display-section">
+                <h2 class="section-title">Development</h2>
+                <p>Thus, to provide an affordable route to environmentally-friendly clothing, I set out to apply the bulk approach to fashion. Initially, bulk wouldn’t seem to work with clothing since it does not account for each person’s unique taste. But, considering many people––despite their differing tastes––share similar articles of basic clothing such as jeans, blazers, turtlenecks, and t-shirts, a window opens for such opportunity. This sustainable, affordable, and high-quality foundation for your wardrobe now allows you to fully focus on purchasing or thrifting eclectic statement pieces that speak to your style.</p>
+            </div>
+            <div class="display-section">
+                <h2 class="section-title">Slide Deck</h2>
+            </div>
+            <div id="pdf"></div>
+
+           
+
+`
+
+
+var aiExplorer = `<div class="project-image" id="header-image" style="--image-url:url('./assets/ai-explorer/web-banner.png')"></div>
+            <div class="display-section">
+                <h2 class="display-date">Summer 2019</h2>
+                <h2 class="section-title">Harvard Art Museums A.I. Explorer</h2>
+            </div>
+            <div class="display-section">
+                    <p>Over the past few years, the Harvard Art Museums had collected data on how artificial intelligence programs interpret artworks. I was given their API (database) of over 14 million machine-generated data-points. During my summer internship, I designed and developed a web-app that is both educational and exploratory for its users, entirely from scratch.</p>
+                    <p>By being the graphic designer, the user experience designer, and the programmer, I gained insight into how to  effectively build and manage a product from the ground up. Overall, I was able to take the helm and create an experience that is clean, intuitive, and, most importantly, human.</p>
+            </div>
+            <div class="display-section">
+                <h2 class="section-title">Background</h2>
+                <p>The Harvard Art Museums' artificial intelligence data comes from five different machine vision services (Google Vision, Microsoft Cognitive Services, Imagga, Clarifai, and Amazon Web Services). Each services gave their own interpretation of each object in the Museum's collection. For instance, if there was a tiger in a painting, the service could identify it, while also taking other guesses as to what it might be. Some of the services could even identify faces, predicting age, gender, and emotion.</p>
+            </div>
+            <div class="display-section">
+                <h2 class="section-title">The Process</h2>
+                <p>First, I had to figure out the overall flow of the website. Wanting the core of the product to be very simple to navigate, I broke it down into a few steps. When the user lands on the homepage, they're introduced to the website's purpose and are then pushed to start exploring. From there, they can search different keywords, like "tiger". In return, they receive the entire body of artworks that have been tagged "tiger" by artificial intelligence, ordered by the artificial intelligence's confidence. They can click on any of these works to get all of its machine data.</p>
+                <p>When deciding what the search results page would look like, I found it important for the user to quickly see if a result is relevant. Thus, I ensured that the confidence (measured by the AI, where 100% demonstrates full conviction in its prediction) was prominently displayed.</p>
+            </div>
+            <div class="display-section">
+                <h2 class="section-title">The Result</h2>
+                <p>I ended my internship with something I'm very proud of! It was important to me that my design appeared simple yet comprehensive. While I faced challenges in displaying data, I worked through it by brainstorming and consulting my peers and coworkers. In the end, I ended up with a product that allows for exploration and even research.</p>
+            </div>`
+
+var novelArt = `<div class="project-image" id="header-image" style="--image-url:url('./assets/novel-art/web-banner.png')"></div>
+            <div class="display-section">
+                <h2 class="display-date">Spring 2020</h2>
+                <h2 class="section-title">Text Visualizer: Novels to Pixel Art</h2>
+            </div>
+            <div class="display-section">
+                    <p>Curious about color in novels, I created a program that parses through an entire body of text and finds each mentioning of a color. Then, after calculating the frequency of each color compared to one another, the program maps out a visualization reminiscent of pixel art.</p>
+            </div>
+            <div class="display-section">
+                <h2 class="section-title">Background</h2>
+                     <p>When you think about books you've read in the past, you might also remember the colors you imagined in your head at the time. For instance, when I remember the Wonderful Wizard of Oz, I think about the yellow brick road and Emerald City. Each book becomes a visual map of colors, where fairytales glow with bright colors, and moody novellas daub themselves in dark hues.
+                    Thus, I sought to visualize colors in novels through code. Using a collection of novels that are free through Project Gutenberg, I created a program that I then applied to books of different genres. Immediately, one can see how different books utilize color in order to invoke a desired mood.</p>
+            </div>
+            <div class="display-section">
+                <h2 class="section-title">The Process</h2>
+                <p>To begin finding colors in novels and displaying the colors visually, I needed a database of colors and their hex codes to begin with. Luckily, the ColorHexa website fulfilled most of my needs, as its comprehensive list includes hex codes, RGB values, hue, saturation, and lightness. Writing a program to scrape the website, I pulled only the color name and its hex code.
+I then went to Project Gutenberg, a collection of free digitized older novels. I selected around 40 works across several genres.</p>
+                <p>With all of the information I needed, I set off to code a program that iterates and parses through each novel, locating each time a color was written. For each novel, I created a Python dictionary, where colors and their frequency were recorded.
+I then reformatted the data for readability, and the image to the left demonstrates the type of data I was retrieving from my program for each book.
+Immediately, I could see how the books wildly differed from each other in their colors mentioned, individual colors frequencies, and total colors. While I could have stopped here, I wanted to make my findings more visual.</p>
+            </div>
+            <div class="display-section">
+                <h2 class="section-title">The Result</h2>
+                <p>Now, the fun part! Inspired by pixel art, I wanted to use all of the data I created above and map it to a square grid. With a program I created, I took the color frequencies data for each novel, and gave each color a ratio based on their frequency compared to other colors within the novel. So, for instance, if the color green appears 23 times and there are 159 total colors, then green will take up 14% of the grid.
+
+Because I wanted my grid to be 80x80, I needed to map all the colors to 1600 total pixels. Using my ratios, I then created a list where each color was repeated 1600*ratio times. For instance, since green takes up 14% of the grid, . I then randomized the list
+‍
+To "draw" this data out, I used the Turtle python package to iterate through the list and draw each square one-by-one, filled with the specified color. For more information on my code, check out the README for this project.</p>
+            </div>`
+
